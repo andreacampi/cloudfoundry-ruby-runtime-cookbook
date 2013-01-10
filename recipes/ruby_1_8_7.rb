2c5007fc22f21b310771b41568ab5fd43222c3b5
Andrea Campi <andrea.campi@zephirworks.com>
2013-01-10 09:44:13 +0100

    Cleanup: remove deprecated recipes.

--- recipes/ruby_1_8_7.rb
+++
@@ -1,2 +0,0 @@
-Chef::Log.warn "cloudfoundry-ruby-runtime::ruby_1_8_7 is deprecated, use cloudfoundry-ruby-runtime::ruby18."
-include_recipe "cloudfoundry-ruby-runtime::ruby18"
--- recipes/ruby_1_9_2.rb
+++
@@ -1,2 +0,0 @@
-Chef::Log.warn "cloudfoundry-ruby-runtime::ruby_1_9_2 is deprecated, use cloudfoundry-ruby-runtime::ruby19."
-include_recipe "cloudfoundry-ruby-runtime::ruby19"
